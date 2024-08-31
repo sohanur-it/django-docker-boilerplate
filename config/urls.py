1a82de21cf10fa9111e65d3987db0571235b02f1
sohanur rahman <shohanur.rahman@gennext.net>
2024-08-31 15:41:26 +0600

    add boilerplate for this django postgres app

---
+++ config/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path, include
+from config import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls.static import static
+
+# Define the schema view for Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Management API",
+        default_version="v1",
+        description="API documentation system",
+        terms_of_service="https://www.yourcompany.com/terms/",
+        contact=openapi.Contact(email="contact@yourcompany.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger.yaml", schema_view.without_ui(cache_timeout=0), name="schema-yaml"),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
